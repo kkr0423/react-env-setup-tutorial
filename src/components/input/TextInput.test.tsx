90c065cd2bd638dd9472ab7527e697f00a69b77d
Ogasawara <kakeru19870423@gmail.com>
2024-10-28 07:14:02 +0900

    mod

--- src/components/input/TextInput.test.tsx
+++ src/components/input/TextInput.test.tsx
@@ -4,7 +4,7 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import TextInput from "./TextInput";
 
-test("TextInput Component test mod", async () => {
+test("TextInput Component test", async () => {
   render(<TextInput />);
   const inputElement = screen.getByRole("textbox");
   expect(inputElement).toBeInTheDocument();
