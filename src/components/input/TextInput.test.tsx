c1f9900f7b46f69be44789742a84e37e301e0478
Ogasawara <kakeru19870423@gmail.com>
2024-11-23 12:03:27 +0900

    Third commit to test for lint-staged

--- src/components/input/TextInput.test.tsx
+++ src/components/input/TextInput.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import TextInput from "./TextInput";
 
-test("TextInput Component test", async () => {
+test("TextInput Component test mod", async () => {
   render(<TextInput />);
 
   const inputEle = screen.getByRole("textbox");
